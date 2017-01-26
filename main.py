5c8a21aef0de3c76b5638aeda81e800c04333854
Jennifer Miller <Jennifer@purplelime.com>
2017-01-26 16:27:19 -0600

    added labels, escaping, rotation key error handling

--- main.py
+++ main.py
@@ -16,17 +16,50 @@
 #
 import webapp2
 import caesar
+import cgi
+
+def build_page(textarea_content, error):
+    header = "<h2>Web Caesar</h2>"
+    message_label = "<label>Message:</label>"
+    textarea = "<textarea name='message'>" + textarea_content + "</textarea>"
+    rot_label = "<label>Rotation Key:</label>"
+    rot_amt = "<input type='number' name='rotation_num' />"
+    submit = "<input type='submit' />"
+    error_msg = "<p><b>" + error + "</b></p>"
+    form = ("<form method='post'>"
+            + header
+            + "<br>"
+            + rot_label
+            + rot_amt
+            + "<br>"
+            + "<br>"
+            + message_label
+            + textarea
+            + "<br>"
+            + submit
+            + error_msg
+            + "</form>")
+
+    return form
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        message = 'Booga booga'
-        encrypted_message = caesar.encrypt(message, 13)
+        content = build_page("", "")
+        self.response.write(content)
 
-        textarea = "<textarea>" + encrypted_message + "</textarea>"
-        submit = "<input type='submit' />"
-        form = "<form>" + textarea + "<br>" + submit + "</form>"
-
-        self.response.write(form)
+    def post(self):
+        message = self.request.get("message")  #gets the info from the message feild
+        rot_num = self.request.get("rotation_num")
+        if rot_num.isdigit():
+            encrypted_message = caesar.encrypt(message, rot_num)
+            escaped_message = cgi.escape(encrypted_message)
+            content = build_page(escaped_message, "")
+            self.response.write(content)
+        else:
+            error_txt = "You must enter a rotation key number"
+            escaped_message = cgi.escape(message)    # because we haven't encrypted yet
+            content = build_page(escaped_message, error_txt)
+            self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
